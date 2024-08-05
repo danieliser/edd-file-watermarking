c808b071210d17b9d9bb99a64644fa07fae75a7c
Daniel Iser <daniel@code-atlantic.com>
2024-08-05 01:57:07 -0400

    Fix PHPCS lint errors

--- edd-file-watermarking.php
+++ edd-file-watermarking.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name: EDD File Watermarking
  * Description: Adds an identifier to downloaded EDD files to track which customer downloaded it. Thanks @westguard!
@@ -20,7 +19,7 @@ namespace EDDFileWatermarking;
 
 require_once __DIR__ . '/inc/functions.php';
 
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
@@ -29,27 +28,27 @@ if (!defined('ABSPATH')) {
  *
  * This calls the `watermark_edd_download` and `watermark_edd_download_{$plugin_filename}` action if the file should be watermarked.
  */
-add_filter('edd_requested_file', __NAMESPACE__ . '\\watermark_edd_download', 10, 4);
+add_filter( 'edd_requested_file', __NAMESPACE__ . '\\watermark_edd_download', 10, 4 );
 
 /**
  * Process the built global & per download settings in watermarks on a given zip file.
  */
-add_action('watermark_edd_download', __NAMESPACE__ . '\\process_zip_builtin_watermarks', 10, 2);
+add_action( 'watermark_edd_download', __NAMESPACE__ . '\\process_zip_builtin_watermarks', 10, 2 );
 
 /**
  * Add global extension settings.
  */
-add_filter('edd_settings_sections_extensions', __NAMESPACE__ . '\\edd_watermark_register_settings_section');
-add_filter('edd_settings_extensions', __NAMESPACE__ . '\\edd_watermark_add_settings');
-add_filter('edd_settings_sanitize_watermark_repeater', __NAMESPACE__ . '\\sanitize_watermark_repeater_settings', 10, 2);
+add_filter( 'edd_settings_sections_extensions', __NAMESPACE__ . '\\edd_watermark_register_settings_section' );
+add_filter( 'edd_settings_extensions', __NAMESPACE__ . '\\edd_watermark_add_settings' );
+add_filter( 'edd_settings_sanitize_watermark_repeater', __NAMESPACE__ . '\\sanitize_watermark_repeater_settings', 10, 2 );
 
 /**
  * Add the watermark meta box.
  */
-add_action('add_meta_boxes', __NAMESPACE__ . '\\edd_watermark_add_meta_box');
-add_action('save_post', __NAMESPACE__ . '\\edd_watermark_save_post_meta');
+add_action( 'add_meta_boxes', __NAMESPACE__ . '\\edd_watermark_add_meta_box' );
+add_action( 'save_post', __NAMESPACE__ . '\\edd_watermark_save_post_meta' );
 
 /**
  * Cleanup old watermarks on a daily basis.
  */
-add_action('edd_daily_scheduled_events', __NAMESPACE__ . '\\edd_watermark_cleanup');
+add_action( 'edd_daily_scheduled_events', __NAMESPACE__ . '\\edd_watermark_cleanup' );
